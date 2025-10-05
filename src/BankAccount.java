b1ca07bc1d1c08cdca8342d82e9af18833db668b
Stepan <afanasevstepan67@gmail.com>
2025-10-05 22:46:24 +0400

    the end.

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,122 @@
-package PACKAGE_NAME;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Random;
 
 public class BankAccount {
+
+    private String name;
+
+    private int balance;
+
+    private LocalDateTime dateOpen;
+
+    private boolean blocked;
+
+    private String number;
+
+    public BankAccount(String name){
+        this.name = name;
+        this.balance = 0;
+        this.dateOpen = LocalDateTime.now();
+        this.blocked = false;
+        this.number = generateNumber();
+    }
+
+    public boolean deposit(int amount){
+        if (blocked || amount <= 0){
+            return false;
+        }
+        else {
+            balance += amount;
+        }
+        return true;
+    }
+
+    public boolean withdraw(int amount){
+        if (amount > balance || amount <= 0 || blocked){
+            return false;
+        }
+        else{
+            balance -= amount;
+        }
+        return true;
+    }
+
+    public boolean transfer(BankAccount otherAccount, int amount){
+        if (this.blocked || otherAccount.blocked){
+            return false;
+        }
+        this.withdraw(amount);
+        otherAccount.deposit(amount);
+        return true;
+    }
+
+    public String generateNumber(){
+        Random random = new Random();
+        StringBuilder str1 = new StringBuilder();
+        for(int i = 0; i < 8; i++){
+            str1.append((random.nextInt(8)));
+        }
+        return str1.toString();
+    }
+
+    @Override
+    public String toString(){
+        return "BankAccount{" +
+                "owner='" + name + '\'' +
+                ", number='" + number + '\'' +
+                ", balance=" + balance +
+                ", openedAt=" + dateOpen +
+                ", blocked=" + blocked +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+        else if(!(obj instanceof BankAccount)){
+            return false;
+        }
+        BankAccount that = (BankAccount) obj;
+        return Objects.equals(number, that.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    public boolean getBlocked(){
+        return blocked;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BankAccount vano = new BankAccount("Vano");
+        BankAccount stepan = new BankAccount("Stepan");
+        BankAccount nasty = new BankAccount("Nasty");
+
+        System.out.println("Создание счета");
+        System.out.println(vano);
+        System.out.println(stepan);
+
+        System.out.println("\nПополнение счета");
+
+        System.out.println("\nБаланс счетов");
+        System.out.println("Пополнение счёта Vano:" + vano.deposit(1000));
+        System.out.println("Пополнение счёта Stepan:" + stepan.deposit(2500));
+
+        System.out.println("\nБаланс Vano:" + vano.getBalance());
+        System.out.println("Баланс Stepan:" + stepan.getBalance());
+
+        System.out.println("\nСнятие денег");
+        System.out.println("Снятие 500 рублей с Vano:" + vano.withdraw(500));
+        System.out.println("Снятие 1000 рублей с Stepan:" + stepan.withdraw(1000));
+
+        System.out.println("\nБаланс Vano:" + vano.getBalance());
+        System.out.println("Баланс Stepan:" + stepan.getBalance());
+
+        System.out.println("\nПеревод денег");
+        System.out.println("Перевод 100 рублей с Vano на Stepan:" + vano.transfer(stepan, 100));
+        System.out.println("Баланс Vano:" + vano.getBalance());
+        System.out.println("Баланс Stepan:" + stepan.getBalance());
+
+        System.out.println("\nСнимаем большее количество денег, чем есть на счету Vano:" + vano.withdraw(5000));
+        nasty.setBlocked(true);
+        System.out.println("\nБлокируем счёт Nasty и пытаемся перевести на заблокированный счёт 300 рублей:" + stepan.transfer(nasty,300));
+
+        System.out.println("\nstepan похож на nasty: " + stepan.equals(nasty));
     }
 }
